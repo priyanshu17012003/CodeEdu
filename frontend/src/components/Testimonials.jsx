76fbfa5e8c80d5c391ec8ec51922052891a4d67d
priyanshu17012003 <priyanshusanki123@gmail.com>
2024-09-29 02:32:23 +0530

    testimonail updated

--- frontend/src/components/Testimonials.jsx
+++ frontend/src/components/Testimonials.jsx
@@ -8,11 +8,11 @@ import testimonials from "../store/testimonials.json";
 function Testimonials() {
   const settings = {
     infinite: true,
-    speed: 800, 
+    speed: 800,
     slidesToShow: 3,
     slidesToScroll: 1,
-    autoplay: true, 
-    autoplaySpeed: 3000, 
+    autoplay: true,
+    autoplaySpeed: 3000,
     cssEase: "linear",
     responsive: [
       {
@@ -35,18 +35,22 @@ function Testimonials() {
   return (
     <>
       <div className="bg-[#1d1d1d] p-10">
-        <h1 className="text-4xl font-bold text-center text-transparent bg-clip-text bg-gradient-to-r from-cyan-300 to-violet-500 mb-4">Testimonials</h1>
+        <h1 className="text-4xl font-bold text-center text-transparent bg-clip-text bg-gradient-to-r from-cyan-300 to-violet-500 mb-4">
+          Testimonials
+        </h1>
         <p className="text-center text-xl text-gray-300 mb-6">
           Here are some of the people who have been using our tool.
         </p>
       </div>
-      <Slider {...settings} className="bg-[#1d1d1d] py-10 px-4">
-        {testimonials.map((item) => (
-          <div key={item.id}>
-            <Card item={item}/>
-          </div>
-        ))}
-      </Slider>
+      <div className="overflow-hidden">
+        <Slider {...settings} className="bg-[#1d1d1d] py-10 px-4">
+          {testimonials.map((item) => (
+            <div key={item.id}>
+              <Card item={item} />
+            </div>
+          ))}
+        </Slider>
+      </div>
     </>
   );
 }
